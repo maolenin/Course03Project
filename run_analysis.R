a950ae9bc4c38ec9917327eb5afa06e58dcf6c57
maolenin <maolenin@gmail.com>
2020-07-14 14:10:38 -0500

    code cleaned

--- run_analysis.R
+++ run_analysis.R
@@ -1,34 +1,38 @@
 #Start of question # 1
 #Loading library to manage data
 library("dplyr")
+library("stringr")
+
 #Opening Features 
-features <- read.table("./data/features.txt");
+features <- read.table("features.txt");
+
 #opening Activities
-alabels <- read.table("./data/activity_labels.txt");
+alabels <- read.table("activity_labels.txt");
 names(alabels) <- c("activity_code", "activity")
+
 #Open data from test file
-stest <- read.table("./data/test/subject_test.txt");
+stest <- read.table("subject_test.txt");
 names(stest) <- "subject";
-xtest <- read.table("./data/test/X_test.txt");
+xtest <- read.table("X_test.txt");
 names(xtest) <- features$V2
-ytest <- read.table("./data/test/y_test.txt");
+ytest <- read.table("y_test.txt");
 names(ytest) <- "activity_code";
 test <- cbind(stest, ytest, "mode" = "test", xtest)
-rm(stest, ytest, xtest)
+rm(stest, ytest, xtest) # Removing variables
 
 #Open data from train file
-strain <- read.table("./data/train/subject_train.txt");
+strain <- read.table("subject_train.txt");
 names(strain) <- "subject";
-xtrain <- read.table("./data/train/X_train.txt")
+xtrain <- read.table("X_train.txt")
 names(xtrain) <- features$V2
-ytrain <- read.table("./data/train/y_train.txt")
+ytrain <- read.table("y_train.txt")
 names(ytrain) <- "activity_code"
 train <- cbind(strain, ytrain, "mode" = "train", xtrain)
-rm(strain, ytrain, xtrain, features)
+rm(strain, ytrain, xtrain, features)  # Removing variables
 
 # Merging tables
 HAR <- rbind(test, train)
-rm(test, train)
+rm(test, train) # Removing variables
 
 #Start of Question # 2
 temp <- select(HAR, subject, activity_code, mode)
@@ -39,26 +43,19 @@ for (var in names(HAR)){
         }
 }
 HAR <- temp
-rm(temp, var, var1)
+rm(temp, var, var1) # Removing variables
 
 #Start of Question # 3
 HAR <- merge(alabels, HAR, by = "activity_code")
-rm(alabels)
+rm(alabels) # Removing variables
 
 #Start of Question # 4
 newnames <- gsub("\\(|,", "-", gsub("\\()|\\)", "", names(HAR)))
 names(HAR) <- newnames
-rm(newnames)
+rm(newnames) # Removing variables
 
 #Start of question # 5
-#HAR3 <- HAR2
-#HAR3 %>%
- #       select(3, 1, 2, 4:90) %>%
-  #      arrange(subject, activity_code) %>%
-   #     group_by(subject, activity_code, activity) %>%
-    #    summarise_all(mean) %>%
-     #   mutate()
 HAR <- select(HAR, 3, 1, 2, 4:90)
 HAR <- arrange(HAR, subject, activity_code)
-HAR <- group_by(HAR, subject, activity_code, activity, mode)
-HARMean <- summarise_all(HAR, mean)
+HARmean <- group_by(HAR, subject, activity_code, activity, mode)
+HARmean <- summarise_all(HARmean, mean)
